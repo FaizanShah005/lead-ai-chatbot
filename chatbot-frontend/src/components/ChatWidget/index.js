a6003acc821febfee5c5934b64d4f29083ee9f57
unknown <faziiishah005@gmail.com>
2025-06-25 08:02:12 -0700

    updated code

--- chatbot-frontend/src/components/ChatWidget/index.js
+++
@@ -1,5 +0,0 @@
-export { default } from './ChatWidget';
-export { default as ChatHeader } from './components/ChatHeader';
-export { default as ChatMessage } from './components/ChatMessage';
-export { default as LeadDisplay } from './components/LeadDisplay';
-export { default as MessageOptions } from './components/MessageOptions'; 
