f8694a6cd8483c07c91517e55efac43a23f14926
unknown <faziiishah005@gmail.com>
2025-07-03 02:03:26 -0700

    Revert debugging changes - restore original working code

--- test_app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'WORKING!'
-
-# Remove health route and if __name__ block to make it as simple as possible 
